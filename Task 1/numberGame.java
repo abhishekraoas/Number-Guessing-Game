ae84028b5d95564830c09e0019c4874d5773d63a
AbhiShek Rao <53809098+abhishekraoas@users.noreply.github.com>
2023-10-26 20:57:54 +0530

    Format the code

--- Task 1/numberGame.java
+++ Task 1/numberGame.java
@@ -1,4 +1,3 @@
-import java.lang.Math;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -7,49 +6,49 @@ public class numberGame {
     // Creating a Function for the game
     static void userGuessNumberGame() {
         while(true){
-            // Creating a Input Object
-            Scanner userInput = new Scanner(System.in);
-    
-            // For Generating Random Number
-            Random randNum = new Random();
-            int randomNumber = randNum.nextInt(100);
-            int yourScore;
-            int chance = 0;
-            int userGuessNumber;
-            int maxAttempt = 10;
-    
-            do {
-                // Creating a Guess Number Logic
-                System.out.print("Enter the Guess Number 1 to 100 : ");
-                userGuessNumber = userInput.nextInt();
-                chance++;
-    
-                // Checking Guess Number
-                if (userGuessNumber == randomNumber) {
-                    System.out.println("Congratulation, You Guess the Correct Number :" + randomNumber);
-                    // Showing the Score out of 100
-                    yourScore = 100 - chance;
-                    System.out.println("Your Score is : " + yourScore);
+            try (
+                Scanner userInput = new Scanner(System.in)) {
+                // For Generating Random Number
+                Random randNum = new Random();
+                int randomNumber = randNum.nextInt(100);
+                int yourScore;
+                int chance = 0;
+                int userGuessNumber;
+                int maxAttempt = 10;
+   
+                do {
+                    // Creating a Guess Number Logic
+                    System.out.print("Enter the Guess Number 1 to 100 : ");
+                    userGuessNumber = userInput.nextInt();
+                    chance++;
+   
+                    // Checking Guess Number
+                    if (userGuessNumber == randomNumber) {
+                        System.out.println("Congratulation, You Guess the Correct Number :" + randomNumber);
+                        // Showing the Score out of 100
+                        yourScore = 100 - chance;
+                        System.out.println("Your Score is : " + yourScore);
+                        break;
+                    } else if (userGuessNumber > randomNumber) {
+                        System.out.println("Your Guess Number is Wrong, That is a Smaller");
+                    } else if (userGuessNumber < randomNumber) {
+                        System.out.println("Your Guess Number is Wrong, That is a Greater");
+                    }
+                } while (chance < maxAttempt);
+   
+                // Attemps the maximum number
+                if (chance == maxAttempt) {
+                    System.out.println("Sorry, You've reached maximum number of attemps. The Correct number is : " + randomNumber);
                     break;
-                } else if (userGuessNumber > randomNumber) {
-                    System.out.println("Your Guess Number is Wrong, That is a Smaller");
-                } else if (userGuessNumber < randomNumber) {
-                    System.out.println("Your Guess Number is Wrong, That is a Greater");
                 }
-            } while (chance < maxAttempt);
-    
-            // Attemps the maximum number
-            if (chance == maxAttempt) {
-                System.out.println("Sorry, You've reached maximum number of attemps. The Correct number is : " + randomNumber);
-                break;
-            }
-    
-            System.out.print("Do You Want Play Again ? Yes or No : ");
-            String playAgain = userInput.next();
-    
-            if (!playAgain.equalsIgnoreCase("yes")) {
-                System.out.println("Thanks For Playing ! ");
-                break;
+   
+                System.out.print("Do You Want Play Again ? Yes or No : ");
+                String playAgain = userInput.next();
+   
+                if (!playAgain.equalsIgnoreCase("yes")) {
+                    System.out.println("Thanks For Playing ! ");
+                    break;
+                }
             }
         }
 
--- Task 2/studentGrade.java
+++ Task 2/studentGrade.java
@@ -3,21 +3,21 @@ import java.util.Scanner;
 public class studentGrade {
     public static void main(String[] args) {
 
-        // Taking Student Input According his/her number of subjects
-        Scanner studentInput = new Scanner(System.in);
-        System.out.println("Enter the Number of Subjects");
-        int totalSubjects = studentInput.nextInt();
+       // Taking Student Input According his/her number of subjects
+         try (
+            Scanner studentInput = new Scanner(System.in)) {
+            System.out.print("Enter the Number of Subjects : ");
+            // Assign Students Marks
+            int totalMarks= 0;
 
-        // Assign Students Marks
-        int totalMarks= 0;
+            for(int i=1; i <= totalMarks; i++){
 
-        for(int i=1; i <= totalMarks; i++){
+                // Taking Student Marks and Adding in totalMarks
+                System.out.println("Enter Your Marks out of 100 in Subject " + i + ": ");
+                int marks = studentInput.nextInt();
+                totalMarks+= marks;
 
-            // Taking Student Marks and Adding in totalMarks
-            System.out.println("Enter Your Marks out of 100 in Subject " + i + ": ");
-            int marks = studentInput.nextInt();
-            totalMarks+= marks;
-
+            }
         }
 
         // Calculating the Percentage
