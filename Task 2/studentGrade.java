3b012cef3f2fd12f22b6db2db07ca7df9c09ddb2
AbhiShek Rao <53809098+abhishekraoas@users.noreply.github.com>
2023-10-29 11:32:00 +0530

    Updated the code

--- Task 2/studentGrade.java
+++ Task 2/studentGrade.java
@@ -1,27 +1,44 @@
 import java.util.Scanner;
-
 public class studentGrade {
     public static void main(String[] args) {
+      try {
+        // Taking Student Input According his/her number of subjects
+        Scanner studentInput = new Scanner(System.in);
+        System.out.print("Enter the Number of Subjects : ");
+        int totalSubjects = studentInput.nextInt();
 
-       // Taking Student Input According his/her number of subjects
-         try (
-            Scanner studentInput = new Scanner(System.in)) {
-            System.out.print("Enter the Number of Subjects : ");
-            // Assign Students Marks
-            int totalMarks= 0;
+        // Assign Students Marks
+        int totalMarks = 0;
 
-            for(int i=1; i <= totalMarks; i++){
+        for (int i = 1; i <= totalSubjects; i++) {
 
-                // Taking Student Marks and Adding in totalMarks
-                System.out.println("Enter Your Marks out of 100 in Subject " + i + ": ");
-                int marks = studentInput.nextInt();
-                totalMarks+= marks;
+            // Taking Student Marks and Adding in totalMarks
+            System.out.print("Enter Your Marks out of 100 in Subject " + i + ": ");
+            int marks = studentInput.nextInt();
+            totalMarks += marks;
 
-            }
         }
 
-        // Calculating the Percentage
+        double studentPercentage = (double)totalMarks / (totalSubjects * 100) * 100;
 
-        // double studentPercentage = (double) 
-    }
+        char grade;
+        if (studentPercentage >= 90) {
+            grade = 'A';
+        } else if (studentPercentage >= 80) {
+            grade = 'B';
+        } else if (studentPercentage >= 70) {
+            grade = 'C';
+        } else {
+            grade = 'D';
+        }
+
+        System.out.println("*---------------------------*");
+        System.out.println("Total Marks : " + totalMarks);
+        System.out.println("Total Percentage : " + studentPercentage);
+        System.out.println("Your Grade is : " + grade);
+        
+      } catch (Exception e) {
+        System.out.println(e.getMessage());
+      }
+    }   
 }
